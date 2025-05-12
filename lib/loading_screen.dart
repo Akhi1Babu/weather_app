f0c34b67d6fe932694109abb8b470d856a6b7aa1
Akhi1Babu <akhilb732@gmail.com>
2025-05-12 13:42:32 +0530

    Enhance loading screen layout and styling for improved user experience

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -16,15 +16,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
         backgroundColor: const Color.fromARGB(255, 38, 36, 36),
         body: Expanded(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(
                 'images/cloud_intro.png',
                 height: 200,
                 width: 200,
               ),
+              const SizedBox(
+                height: 50,
+              ),
               Container(
+                padding: EdgeInsets.symmetric(horizontal: 30),
                 child: const Text(
                   'Discover the Weather in your city',
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                     fontFamily: 'Poppins-bold',
                     fontSize: 25,
@@ -32,6 +39,47 @@ class _LoadingScreenState extends State<LoadingScreen> {
                     fontWeight: FontWeight.w600,
                   ),
                 ),  
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  
+                  "Get the latest weather updates and forecasts for your location.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Poppins-regular',
+                    fontSize: 15,
+                    color: Colors.grey[400],
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: (){},
+                child: Container(
+                  margin: EdgeInsets.all(30),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[600],
+                    borderRadius: BorderRadius.circular(16),
+                    
+                    
+                  ),
+                  child: Center(
+                    child: Text("get started",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Poppins-regular',
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
               )
           
             ],
